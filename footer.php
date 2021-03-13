c187fdaee20d087d77221be3a0a132e1bbb0ad82
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-14 08:17:44 +0900

    move code from index.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="wrapper">
+  <ul>
+    <li><a href="https://www.instagram.com" target="_blank">instagram</a></li>
+    <li><a href="https://twitter.com" target="_blank">twitter</a></li>
+    <li><a href="https://www.facebook.com" target="_blank">facebook</a></li>
+  </ul>
+  <p>&copy; Furniture Design</p>
+</footer>
+
+</body>
+</html>
