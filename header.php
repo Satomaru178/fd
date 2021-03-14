69d9b7153c0f268824230d1b50a02e724e30c33c
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-14 09:14:28 +0900

    wp_head

--- header.php
+++ header.php
@@ -13,6 +13,8 @@
   <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="js/main.js"></script>
+
+  <?php wp_head(); ?>
 </head>
 
 <body>
