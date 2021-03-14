1b9650c32928e12ca4d5b01f050a4a2e12e85a2d
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-14 09:13:42 +0900

    Create functions.php and read css and js

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**************************************************
+CSSファイルの読み込み
+ **************************************************/
+function my_enqueue_styles() {
+  wp_register_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
+  wp_register_style('style', get_stylesheet_uri(), array('ress'), false, 'all');
+  wp_enqueue_style('style');
+  wp_enqueue_script('base-script', get_theme_file_uri('/js/main.js'), array('jquery'), false);
+}
+add_action('wp_enqueue_scripts', 'my_enqueue_styles');
+
