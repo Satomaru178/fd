f35e369b6a0e7ee8497564152f7196727673b76b
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-16 21:51:12 +0900

    move cat and catname variable

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
       <?php query_posts('posts_per_page=8'); ?>
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <?php
-            $cat = get_the_category();
-            $catname = $cat[0]->cat_name;
+          $cat = get_the_category();
+          $catname = $cat[0]->cat_name;
           ?>
           <li class="item">
             <a href="<?php the_permalink(); ?>">
