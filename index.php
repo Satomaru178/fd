096c9ce09d54b9d4ef7f2f36f08c4ff45e7e5599
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-16 21:50:58 +0900

    set post per page

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-  <main id="main">
-    <section id="recommend" class="wrapper">
-      <ul class="grid">
-        <?php if(have_posts()): while(have_posts()): the_post(); ?>
+<main id="main">
+  <section id="recommend" class="wrapper">
+    <ul class="grid">
+      <?php query_posts('posts_per_page=8'); ?>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <?php
             $cat = get_the_category();
             $catname = $cat[0]->cat_name;
