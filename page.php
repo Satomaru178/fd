6a97363153015c2d170544ac4c7fa569128b831a
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-17 14:45:21 +0900

    Create page.php and write wordpress style

--- page.php
+++ page.php
@@ -1,4 +1,14 @@
 <?php get_header(); ?>
 
+  <main id="main">
+    <?php
+    $slug = basename(get_permalink());
+    ?>
+    <section id="<?php echo $slug ?>" class="wrapper">
+      <h3 class="sec-title"><?php the_title(); ?></h3>
+
+      <?php the_content(); ?>
+    </section>
+  </main>
 
 <?php get_footer(); ?>
